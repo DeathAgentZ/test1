e345515a229e6bee27a81875754e6f5b747d81f0
DeathAgentZ <2696051161@qq.com>
2020-10-11 01:51:07 +0800

    测试

---
+++ src/main/java/com/zsy/gitstart/GitStartApplication.java
@@ -0,0 +1,13 @@
+package com.zsy.gitstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zsy/gitstart/GitStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsy.gitstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
